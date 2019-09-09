0d051c18ddb05a07e0923507fb27f49ddb070690
郑小波 <zxiaobo@linewell.com>
2019-09-09 17:47:10 +0800

    feat: 新增rollup打包配置

---
+++ config/rollup.config.js
@@ -0,0 +1,52 @@
+import path from 'path'
+
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from 'rollup-plugin-commonjs';
+// import { uglify } from "rollup-plugin-uglify";
+import autoprefixer from 'autoprefixer'
+
+const paths = {
+  input: path.resolve(__dirname, '../src/lib/index.js')
+}
+
+export default {
+  input: paths.input,
+  external: ['react', 'react-dom', 'antd'],
+  output: [{
+    name: 'RuleEditor',
+    format: 'es',
+    file: 'dist/es/index.js',
+    globals: {
+      react: 'React',
+      "react-dom": "ReactDOM",
+      "antd": "antd"
+    }
+  }, {
+    name: 'RuleEditor',
+    format: 'umd',
+    file: 'dist/lib/index.js',
+    globals: {
+      react: 'React',
+      "react-dom": "ReactDOM",
+      "antd": "antd"
+    }
+  }],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: '**/node_modules/**',
+      runtimeHelpers: true
+    }),
+    commonjs(),
+    postcss({
+      // extract: true,
+      extensions: ['.scss'],
+      plugins: [
+        autoprefixer()
+      ]
+    }),
+    // uglify()
+  ]
+}
